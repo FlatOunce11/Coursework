954d2dcb84baef0cc617fe35b16fbc5c93edc240
FlatOunce11@debian <roman.2006y@gmail.com>
2026-05-29 02:09:12 +0700

    add gitignore. start project

--- main.cpp
+++ main.cpp
@@ -7,22 +7,5 @@
 
 int main(int argc, char *argv[]) {
     QApplication trips(argc, argv);
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("trips.db");
-    db.open();
-    QSqlQuery query;
-    query.exec("CREATE TABLE IF NOT EXISTS departments ("
-               "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-               "name TEXT, phone TEXT)");
-    QMainWindow window;
-    QTableView *tableView = new QTableView(&window);
-    QSqlTableModel *model = new QSqlTableModel(nullptr);
-    model->setTable("departments");  
-    model->select();
-    tableView->setModel(model);
-    window.setCentralWidget(tableView);
-    window.resize(800, 700);
-    window.show();
 
-    return trips.exec();
 }
