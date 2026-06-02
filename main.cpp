adb84ecbd5de5af2e7371cc696d56da26df6f5fc
FlatOunce11@debian <roman.2006y@gmail.com>
2026-06-02 19:38:26 +0700

    Отказ от  CRTL+R, Завершение программы, если БД не открылась. БД создается и заполняется самостоятельно. Данные добавляются или заменятются по ID в таблице. Добавлены три вкладки, по одной на таблицу

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 #include <QSqlQuery>
 #include <QMainWindow>
 #include <QShortcut>
+#include <QTabWidget>
+#include <QTableView>
+#include <QSqlTableModel>
 
 void initdb(QSqlDatabase &tripsdb){
     QSqlQuery tripsquery(tripsdb);
-    tripsdb.open();
     tripsquery.exec(
         "CREATE TABLE IF NOT EXISTS departments ("
         "id INTEGER PRIMARY KEY AUTOINCREMENT," 
@@ -24,7 +26,7 @@ void initdb(QSqlDatabase &tripsdb){
         "departament_id INTEGER,"
         "address TEXT,"
         "phone_number TEXT,"
-        "FOREIGN KEY(departament_id) REFERENCES departaments(id)"
+        "FOREIGN KEY(departament_id) REFERENCES departments(id)"
         ")"
     );
     tripsquery.finish();
@@ -43,48 +45,47 @@ void initdb(QSqlDatabase &tripsdb){
     );
     tripsquery.finish();
     tripsquery.exec(
-        "INSERT INTO departments(name, phone_number) VALUES"
-        "('Отдел кадров', '+7-391-151-00-01'),"
-        "('Отдел дизайна', '+7-391-151-00-02'),"
-        "('Отдел програмирования', '+7-391-151-00-03');"
+        "INSERT OR REPLACE INTO departments(id, name, phone_number) VALUES"
+        "(1, 'Отдел кадров', '+7-391-151-00-01'),"
+        "(2, 'Отдел дизайна', '+7-391-151-00-02'),"
+        "(3, 'Отдел програмирования', '+7-391-151-00-03');"
     );
     tripsquery.finish();
     tripsquery.exec(
-        "INSERT INTO workers(surname, name, patronymic, departament_id, address, phone_number) VALUES"
-        "('Ковальский', 'Алексей', 'Валерьевич', '1', 'ул. Кельвина, д. 506', '+7-392-151-01-01'),"
-        "('Осипов', 'Олег', 'Васильевич', '1', 'ул. Ньюэлла, д. 2', '+7-392-151-01-02'),"
-        "('Киселев', 'Дмитрий', 'Сергеевич', '1', 'ул. Платиновая, д. 300', '+7-392-151-01-03'),"
-        "('Галочкин', 'Максим', 'Сергеевич', '2', 'ул. Баумана, д. 46', '+7-392-151-02-01'),"
-        "('Брагин', 'Антон', 'Александрович', '2', 'ул. Телевизорная, д. 215', '+7-392-151-02-02'),"
-        "('Андреев', 'Фёдор', 'Александрович', '2', 'ул. Дорожная, д. 85', '+7-392-151-02-03'),"
-        "('Кучеров', 'Олег', 'Александрович', '3', 'ул. Калинина, д. 111', '+7-392-151-03-01'),"
-        "('Черепанов', 'Андрей', 'Андреевич', '3', 'ул. Марковского, д. 92', '+7-392-151-03-02'),"
-        "('Шиманович', 'Никита', 'Дмитриевич', '3', 'ул. Минская, д. 17', '+7-392-151-03-03');"
+        "INSERT OR REPLACE INTO workers(id, surname, name, patronymic, departament_id, address, phone_number) VALUES"
+        "(1, 'Ковальский', 'Алексей', 'Валерьевич', '1', 'ул. Кельвина, д. 506', '+7-392-151-01-01'),"
+        "(2, 'Осипов', 'Олег', 'Васильевич', '1', 'ул. Ньюэлла, д. 2', '+7-392-151-01-02'),"
+        "(3, 'Киселев', 'Дмитрий', 'Сергеевич', '1', 'ул. Платиновая, д. 300', '+7-392-151-01-03'),"
+        "(4, 'Галочкин', 'Максим', 'Сергеевич', '2', 'ул. Баумана, д. 46', '+7-392-151-02-01'),"
+        "(5, 'Брагин', 'Антон', 'Александрович', '2', 'ул. Телевизорная, д. 215', '+7-392-151-02-02'),"
+        "(6, 'Андреев', 'Фёдор', 'Александрович', '2', 'ул. Дорожная, д. 85', '+7-392-151-02-03'),"
+        "(7, 'Кучеров', 'Олег', 'Александрович', '3', 'ул. Калинина, д. 111', '+7-392-151-03-01'),"
+        "(8, 'Черепанов', 'Андрей', 'Андреевич', '3', 'ул. Марковского, д. 92', '+7-392-151-03-02'),"
+        "(9, 'Шиманович', 'Никита', 'Дмитриевич', '3', 'ул. Минская, д. 17', '+7-392-151-03-03');"
     );
     tripsquery.finish();
     tripsquery.exec(
-        "INSERT INTO trips(worker_id, city, departure_date, count_days, allowance, price) VALUES"
-        "(1, 'Тольятти', '2026-03-12', 3, 2300, 15000),"
-        "(1, 'Киров', '2026-03-13', 5, 4700, 23400),"
-        "(2, 'Пермь', '2026-03-14', 4, 850, 26000),"
-        "(2, 'Ижевск', '2026-03-15', 3, 6300, 40000),"
-        "(3, 'Екатеринбург', '2026-03-17', 3, 1500, 8500),"
-        "(3, 'Челябинск', '2026-03-18', 2, 1750, 8600),"
-        "(4, 'Уфа', '2026-03-20', 4, 4800, 24500),"
-        "(4, 'Оренбург', '2026-03-21', 2, 1400, 56000),"
-        "(5, 'Магнитогорск', '2026-03-22', 1, 1500, 32000),"
-        "(5, 'Курган', '2026-03-24', 1, 2600, 26800),"
-        "(6, 'Тюмень', '2026-03-25', 1, 2300, 15000),"
-        "(6, 'Омск', '2026-03-26', 6, 4700, 23400),"
-        "(7, 'Новосибирск', '2026-03-28', 7, 850, 26000),"
-        "(7, 'Барнаул', '2026-03-29', 4, 6300, 40000),"
-        "(8, 'Новокузнецк', '2026-03-31', 4, 1500, 85000),"
-        "(8, 'Кемерово', '2026-04-01', 3, 1750, 81600),"
-        "(9, 'Томск', '2026-04-02', 9, 4800, 24500),"
-        "(9, 'Красноярск', '2026-04-05', 5, 6000, 27000);"
+        "INSERT OR REPLACE INTO trips(id, worker_id, city, departure_date, count_days, allowance, price) VALUES"
+        "(1, 1, 'Тольятти', '2026-03-12', 3, 2300, 15000),"
+        "(2, 1, 'Киров', '2026-03-13', 5, 4700, 23400),"
+        "(3, 2, 'Пермь', '2026-03-14', 4, 850, 26000),"
+        "(4, 2, 'Ижевск', '2026-03-15', 3, 6300, 40000),"
+        "(5, 3, 'Екатеринбург', '2026-03-17', 3, 1500, 8500),"
+        "(6, 3, 'Челябинск', '2026-03-18', 2, 1750, 8600),"
+        "(7, 4, 'Уфа', '2026-03-20', 4, 4800, 24500),"
+        "(8, 4, 'Оренбург', '2026-03-21', 2, 1400, 56000),"
+        "(9, 5, 'Магнитогорск', '2026-03-22', 1, 1500, 32000),"
+        "(10, 5, 'Курган', '2026-03-24', 1, 2600, 26800),"
+        "(11, 6, 'Тюмень', '2026-03-25', 1, 2300, 15000),"
+        "(12, 6, 'Омск', '2026-03-26', 6, 4700, 23400),"
+        "(13, 7, 'Новосибирск', '2026-03-28', 7, 850, 26000),"
+        "(14, 7, 'Барнаул', '2026-03-29', 4, 6300, 40000),"
+        "(15, 8, 'Новокузнецк', '2026-03-31', 4, 1500, 85000),"
+        "(16, 8, 'Кемерово', '2026-04-01', 3, 1750, 81600),"
+        "(17, 9, 'Томск', '2026-04-02', 9, 4800, 24500),"
+        "(18, 9, 'Красноярск', '2026-04-05', 5, 6000, 27000);"
     );
-    tripsquery.finish();        
-    tripsdb.close();
+    tripsquery.finish();
 }
 
 int main(int argc, char *argv[]) {
@@ -96,11 +97,30 @@ int main(int argc, char *argv[]) {
     QSqlQuery tripsquery(tripsdb);
     const char *dbPath = "trips.db";
     tripsdb.setDatabaseName(dbPath);
-    new QShortcut(QKeySequence("Ctrl+R"), &tripsWindow, [&]() { initdb(tripsdb); });
-    tripsWindow.setWindowTitle("Командировки");
+    if (!tripsdb.open()) return 1;
+    initdb(tripsdb); 
+    QTabWidget *tripsWindowTab = new QTabWidget(&tripsWindow);
+    QSqlTableModel *depatrmentsTable = new QSqlTableModel(nullptr, tripsdb);
+    depatrmentsTable->setTable ("departments");
+    depatrmentsTable->select();
+    QTableView *depatrmentsTab = new QTableView;
+    depatrmentsTab->setModel(depatrmentsTable);
+    tripsWindowTab->addTab(depatrmentsTab, "Отделы");
+    QSqlTableModel *workersTable = new QSqlTableModel(nullptr, tripsdb);
+    workersTable->setTable ("workers");
+    workersTable->select();
+    QTableView *workersTab = new QTableView;
+    workersTab->setModel(workersTable);
+    tripsWindowTab->addTab(workersTab, "Сотрудники");
+    QSqlTableModel *tripsTable = new QSqlTableModel(nullptr, tripsdb);
+    tripsTable->setTable ("trips");
+    tripsTable->select();
+    QTableView *tripsTab = new QTableView;
+    tripsTab->setModel(tripsTable);
+    tripsWindowTab->addTab(tripsTab, "Командировки");        
+    tripsWindow.setWindowTitle("Курсовая Работа - БД Командировки");
     tripsWindow.resize(800, 600);
     tripsWindow.show();
-
-
+    tripsWindow.setCentralWidget(tripsWindowTab);
     return trips.exec();
 }
